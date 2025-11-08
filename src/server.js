620ac84ca7cb3e8496a5c699d1bb3ff334509297
murilopava <murilopava@gmail.com>
2025-11-08 16:24:58 -0300

    feat: conectar o servidor à pasta public

--- src/server.js
+++ src/server.js
@@ -1,17 +1,36 @@
 import { fastify } from 'fastify'
+import cors from '@fastify/cors'
+import { fastifyStatic } from '@fastify/static'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import { DatabaseAlunos } from './database/alunos-database.js'
 import { alunosRoutes } from './routes/alunos-routes.js'
 import { DatabaseProfessores } from './database/professores-database.js'
 import { professoresRoutes } from './routes/professores-routes.js'
 
 const server = fastify()
+
+await server.register(cors, {origin: true})
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+await server.register(fastifyStatic, {
+    root: path.join(__dirname, '..', '/public'),
+    prefix: '/'
+})
+
 const dbAlunos = new DatabaseAlunos
 const dbProfessores = new DatabaseProfessores
 
 alunosRoutes(server, dbAlunos)
 professoresRoutes(server, dbProfessores)
 
-server.listen({
-    message: "Porta 3000",
-    port: 3000
-})
+try {
+    server.listen({
+        port: 3000},
+    console.log('Rodando na porta 3000'))
+} catch(err) {
+    console.error('Falha ao iniciar o servidor, erro: ', err)
+    process.exit(1)
+}
